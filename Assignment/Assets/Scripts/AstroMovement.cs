a337c18ee1f91c7c058e2895c475dbea0ba6b21b
BiancaPalileo <biclpalileo@gmail.com>
2021-12-03 11:01:40 +0000

    set camera and started astronaut movement - 30 minutes

---
+++ Assignment/Assets/Scripts/AstroMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rigidbody;
+
+    private void FixedUpdate() {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        rigidbody.MovePosition(rigidbody.position + forwardMove);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
