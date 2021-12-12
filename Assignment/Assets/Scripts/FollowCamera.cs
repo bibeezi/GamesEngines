226f30c2a220c5ae389beac5872ac0d831555929
BiancaPalileo <biclpalileo@gmail.com>
2021-12-12 19:14:24 +0000

    Create stars background using particle system - 45 minutes

---
+++ Assignment/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = target.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = target.rotation;
+    }
+}
