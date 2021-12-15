be24e5b40848effbd0e39c5254f232a8487dd00f
BiancaPalileo <biclpalileo@gmail.com>
2021-12-15 17:41:21 +0000

    Created nebula spawner and testing particle system options - 1 hour

---
+++ Assignment/Assets/Scripts/NebulaSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NebulaSpawner : MonoBehaviour
+{
+    public ParticleSystem nebula;
+    public int nebulaAmount = 1;
+
+    void Start()
+    {
+        for(int i = 0; i < nebulaAmount; i++)
+        {
+            Instantiate(nebula, new Vector3(120, 0, 100), Quaternion.identity);
+        }
+    }
+}
