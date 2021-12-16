3c7b7dec4235c99a9061ffb2abe8110a1234f59c
BiancaPalileo <biclpalileo@gmail.com>
2021-12-16 11:23:38 +0000

    Astronaut controller, horizontal and forward movement finished, fixed looking bugs - 45 minutes

--- Assignment/Assets/Scripts/AstroController.cs
+++ Assignment/Assets/Scripts/AstroController.cs
@@ -4,27 +4,48 @@ using UnityEngine;
 
 public class AstroController : MonoBehaviour
 {
-    public GameObject astronaut;
-    public float lookSpeed = 100f;
-    public Transform transform;
-    public float angleX = 0;
-    public float angleY = 0;
+    Transform transform;
+    Rigidbody rigidbody;
+    public float lookSpeed = 0.5f;
+    public float speed = 30f;
+    float angleX = 0;
+    float angleY = 0;
+
 
     void Start()
     {
         Cursor.visible = false;
         transform = gameObject.transform;
+        rigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        angleX = angleX + mouseX;
-        angleY = angleY + mouseY;
+        angleX = angleX + (mouseX % 1);
+        angleY = angleY + (mouseY % 1);
+        
+        transform.localRotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(-angleY, angleX, 0), lookSpeed * Time.deltaTime);
 
-        Quaternion smoothRotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(-angleY, angleX, 0), lookSpeed * Time.deltaTime);
-        transform.localRotation = smoothRotation;
+        if(x > 0)
+        {
+            rigidbody.AddForce(transform.right * speed * Time.deltaTime);
+        }
+        else if(x < 0)
+        {
+            rigidbody.AddForce(-transform.right * speed * Time.deltaTime);
+        }
+        if(z > 0)
+        {
+            rigidbody.AddForce(transform.forward * speed * Time.deltaTime);
+        }
+        else if(z < 0)
+        {
+            rigidbody.AddForce(-transform.forward * speed * Time.deltaTime);
+        }
     }
 }
